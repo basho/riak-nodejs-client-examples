a40a9670669e61ab326a46131053d1fc4ffa1704
Luke Bakken <lbakken@basho.com>
2015-04-06 12:55:17 -0700

    more examples for dev/using/basics

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 'use strict';
 
+/*
+ * NOTE:
+ * Be sure to run ./tools/devrel/setup-examples from the
+ * riak-nodejs-client repo prior to running these. It will
+ * create the expected bucket types and other settings.
+ */
+
 var TasteOfRiakIntroduction = require('./dev/taste-of-riak/introduction');
+var DevUsingBasics = require('./dev/using/basics');
 
 TasteOfRiakIntroduction();
+
+DevUsingBasics();
