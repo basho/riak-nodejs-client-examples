132a84ccfe480d8116eddef58992287ea8dfad2b
Luke Bakken <lbakken@basho.com>
2015-04-08 01:17:51 -0700

    adding nodejs examples from dev/using/data-types

--- dev/using/data-types.js
+++ dev/using/data-types.js
@@ -8,45 +8,59 @@ var config = require('../../config');
  */
 
 var async = require('async');
+var clone = require('clone');
 var logger = require('winston');
 var Riak = require('basho-riak-client');
 
 function DevUsingDataTypes() {
     var client = config.createClient();
 
-    create_counter();
+    var default_options = {
+        bucketType: 'counters',
+        bucket: 'counter',
+        key: 'traffic_tickets'
+    };
 
-    function create_counter() {
-        client.updateCounter({
-                bucketType: 'counters',
-                bucket: 'counters',
-                key: 'traffic_tickets',
-                increment: 1
-            }, function (err, rslt) {
+    create_or_update_counter(default_options, 1, function(opt1) {
+        display_counter(opt1, function(opt2) {
+            create_or_update_counter(opt2, -1, function(opt3) {
+                display_counter(opt3);
+            });
+        });
+    });
+
+    function create_or_update_counter(options, amount, next_func) {
+        var my_opts = clone(options);
+        my_opts.increment = amount;
+        client.updateCounter(my_opts,
+            function (err, rslt) {
                 if (err) {
                     throw new Error(err);
                 }
-                display_counter('counters', 'counter', 'traffic_tickets');
+                if (next_func) {
+                    next_func(options);
+                }
             }
         );
     }
 
-    function display_counter(bucketType, bucket, key) {
-        client.fetchCounter({
-                bucketType: 'counters',
-                bucket: 'counters',
-                key: 'traffic_tickets'
-            }, function (err, rslt) {
+    function display_counter(options, next_func) {
+        client.fetchCounter(options,
+            function (err, rslt) {
                 if (err) {
                     throw new Error(err);
                 }
 
                 if (rslt.notFound) {
-                    logger.error("bt: %s, b: %s, k: %s, counter: NOT FOUND",
-                        bucketType, bucket, key);
+                    logger.error("[DevUsingDataTypes] bt: %s, b: %s, k: %s, counter: NOT FOUND",
+                        options.bucketType, options.bucket, options.key);
                 } else {
-                    logger.info("bt: %s, b: %s, k: %s, counter: %d",
-                        bucketType, bucket, key, rslt.counterValue);
+                    logger.info("[DevUsingDataTypes] bt: %s, b: %s, k: %s, counter: %d",
+                        options.bucketType, options.bucket, options.key, rslt.counterValue);
+                }
+
+                if (next_func) {
+                    next_func(options);
                 }
             }
         );
